76a40dc628d08c585e99e71835877ff316e7b5cb
Mike Ma <mike@hitthespot.com>
2014-03-06 17:12:35 -0800

    Removing the precompile line from application.rb

--- config/application.rb
+++ config/application.rb
@@ -50,8 +50,5 @@ module Hitthespot
     } unless Rails.env.production?
     config.middleware.insert_after 'Rack::Cache', 'Dragonfly::Middleware', :images
 
-    # Heroku
-    config.assets.initialize_on_precompile = false
-    
   end
 end
